1a8257a0da058609a44e0f2b2c59a66428a5dce5
Vamshi Krishna <vamshikrishnabandanakanti@gmail.com>
2026-04-23 22:19:19 +0530

    all up to date1

--- frontend/src/components/workflow/demo.tsx
+++ frontend/src/components/workflow/demo.tsx
@@ -1,5 +1,7 @@
 import { N8nWorkflowBlock } from "@/components/ui/n8n-workflow-block-shadcnui"
 // import { N8nWorkflowBlock } from "@/components/ui/n8n-workflow-block-shadcnui"
+// import { N8nWorkflowBlock } from "@/components/ui/n8n-workflow-block-shadcnui"
+
 export default function Demo() {
   return (
     <div className="flex min-h-[calc(100vh-120px)] w-full items-center justify-center p-4 md:p-8">
